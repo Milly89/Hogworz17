2bf6ec7b5941529490141ebfa7dea2115a035313
Milly89 <meilinfan@163.com>
2021-01-30 04:57:53 -0800

    Pytest实战一作业

--- pythoncode/Calculator.py
+++ pythoncode/Calculator.py
@@ -5,5 +5,11 @@ class Calculator:
     def add(self, a, b):
         return a + b
 
+    def Multi(self, a, b):
+        return a * b
+
     def div(self, a, b):
-        return a / b
+        if b == 0:
+            print("分母为0，输入不合法！")
+        else:
+            return a / b
--- testing/test_calculator.py
+++ testing/test_calculator.py
@@ -17,19 +17,33 @@ def get_datas():
     return (datas['add']['datas'], datas['add']['ids'])
 
 
+def get_MultiDatas():
+    with open("./datas/calc.yml") as f:
+        multiDatas = yaml.safe_load(f)
+        return (multiDatas['Multi']['datas'], multiDatas['Multi']['ids'])
+
+
+def get_DivDatas():
+    with open("./datas/calc.yml") as f:
+        divDatas = yaml.safe_load(f)
+        return (divDatas['Div']['datas'], divDatas['Div']['ids'])
+
 # yaml json excel csv xml
 # 测试类
 class TestCalc:
     datas: list = get_datas()
 
+    Multi_datas = get_MultiDatas()
+    Div_datas = get_DivDatas()
+
     # 前置条件
-    def setup_class(self):
-        print("开始计算")
+    def setup(self):
+        print("方法开始计算")
         self.calc = Calculator()
 
     # 后置条件
-    def teardown_class(self):
-        print("结束计算")
+    def teardown(self):
+        print("方法结束计算")
 
     @pytest.mark.parametrize("a, b, result", datas[0], ids=datas[1])
     def test_add(self, a, b, result):
@@ -37,12 +51,17 @@ class TestCalc:
         assert result == self.calc.add(a, b)
 
     def test_add1(self):
-        datas = [[1, 1, 2], [100, 400, 300], [1, 0, 1]]
+        datas = [[1, 1, 2], [100, 200, 300], [1, 0, 1]]
         for data in datas:
             print(data)
             assert data[2] == self.calc.add(data[0], data[1])
 
-    # TODO: 完善相加功能
-    # TODO: 相除功能
-    def test_div(self):
-        pass
+    @pytest.mark.parametrize("a, b, result", Multi_datas[0], ids=Multi_datas[1])
+    def test_Multi(self, a, b, result):
+        print(f"Test muti function: first one is : {a}, the second one is : {b}")
+        assert result == self.calc.Multi(a, b)
+
+    @pytest.mark.parametrize("a, b, result", Div_datas[0], ids=Div_datas[1])
+    def test_div(self, a, b, result):
+        print(f"Test div function: first num is : {a}, the second num is : {b}")
+        assert result == self.calc.div(a, b)
